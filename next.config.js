fe002019436c0954bfb152fcacb9c37686803623
mijan-alt <mijanigoni@gmail.com>
2026-03-02 11:18:53 +0100

    explicity added the r2 domain

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 
 import redirects from './redirects.js'
-const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL 
+  || (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000')
 
 
 console.log('SERVER_URL:', NEXT_PUBLIC_SERVER_URL)
@@ -20,7 +19,7 @@ const nextConfig = {
       // Cloudflare R2
       {
         protocol: 'https',
-        hostname: process.env.R2_DOMAIN || '',
+        hostname: process.env.R2_DOMAIN || 'pub-617933b153804484919b6cd0b425966c.r2.dev',
       },
 
       {
